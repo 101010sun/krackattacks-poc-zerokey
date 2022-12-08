aebdc779f47284746861bd66041a7c25ebca7aa0
101010sun <10sun19991119@gmail.com>
2022-12-08 15:33:12 +0800

    fix: name error

--- krackattack/krack-all-zero-tk.py
+++ krackattack/krack-all-zero-tk.py
@@ -272,12 +272,12 @@ def append_csa(p, channel, count=1):
 
 	return p
 
-def get_tlv_value(p, type):
+def get_tlv_value(p, typee):
 	print(type(p))
 	if not Dot11Elt in p: return None
 	el = p[Dot11Elt]
 	while isinstance(el, Dot11Elt):
-		if el.ID == type:
+		if el.ID == typee:
 			return el.info
 		el = el.payload
 	return None
