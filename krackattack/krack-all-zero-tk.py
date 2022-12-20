edc97d222102726af121a18f4bcebed2ee7dab38
101010sun <10sun19991119@gmail.com>
2022-12-20 18:09:28 +0800

    feat: try try

--- krackattack/krack-all-zero-tk.py
+++ krackattack/krack-all-zero-tk.py
@@ -513,6 +513,7 @@ class KRAckAttack():
 				print('513: ', end='')
 				print(p)
 				for pp in p:
+					print('check p')
 					if pp.haslayer(Dot11):
 						if pp.type == 0 and pp.subtype == 8:
 							print('p has layer')
