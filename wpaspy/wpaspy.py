87a3d57f3f0ee282a0cb33ff1d688c963b92825c
101010sun <10sun19991119@gmail.com>
2023-01-10 16:03:58 +0800

    feat: encode

--- wpaspy/wpaspy.py
+++ wpaspy/wpaspy.py
@@ -86,9 +86,9 @@ class Ctrl:
 
     def request(self, cmd, timeout=10):
         if self.udp:
-            self.s.sendto(self.cookie + cmd.decode(), self.sockaddr)
+            self.s.sendto(self.cookie + cmd.encode(), self.sockaddr)
         else:
-            self.s.send(cmd.decode())
+            self.s.send(cmd.encode())
         [r, w, e] = select.select([self.s], [], [], timeout)
         if r:
             return self.s.recv(4096)
