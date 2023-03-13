8039c3cfcbf2038e13d76c079eecc8e9f3aa1a30
611410066 <10sun19991119@gmail.com>
2023-03-13 16:26:10 +0800

    try:

--- krackattack/test.py
+++ krackattack/test.py
@@ -1,4 +1,4 @@
 from scapy.all import *
 
-ps = sniff(count=1, timeout=0.5, prn=lambda x: x.summary(), iface=['wlan1'])
+ps = sniff(count=1, timeout=10, prn=lambda x: x.summary(), iface=['wlan1'])
 print(ps)
