9811b8f6bd3122e59b76faa83ca9f48dc069f1e4
611410066 <10sun19991119@gmail.com>
2023-03-13 17:20:21 +0800

    try;

--- krackattack/test.py
+++ krackattack/test.py
@@ -1,4 +1,4 @@
 from scapy.all import *
 
-ps = sniff(count=100, timeout=10, lfilter=lambda p: p.haslayer(Dot11Beacon), prn=lambda x: x.summary(), iface=['wlan0'])
+ps = sniff(count=100, timeout=10, lfilter=lambda p: p.haslayer(Dot11Beacon), prn=lambda x: x.summary(), iface=['wlan1'])
 print(ps)
