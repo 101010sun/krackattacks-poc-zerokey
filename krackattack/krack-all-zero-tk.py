b4a847d95d80425ba6ea8da8ddbaf631189cc0a6
611410066 <10sun19991119@gmail.com>
2023-05-01 13:31:20 +0800

    fix: popen forget list

--- krackattack/krack-all-zero-tk.py
+++ krackattack/krack-all-zero-tk.py
@@ -618,7 +618,7 @@ class KRAckAttack():
 		with open(os.path.join(self.script_path, "../hostapd/hostapd_rogue.conf"), "w") as fp:
 			fp.write(self.netconfig.write_config(self.nic_rogue_ap))
 		
-		self.hostapd = subprocess.Popen(os.path.join(self.script_path, "../hostapd/hostapd"), os.path.join(self.script_path, "../hostapd/hostapd_rogue.conf"), "-dd", "-K", stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
+		self.hostapd = subprocess.Popen([os.path.join(self.script_path, "../hostapd/hostapd"), os.path.join(self.script_path, "../hostapd/hostapd_rogue.conf"), "-dd", "-K"], stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
 		self.hostapd_log = open("hostapd_rogue.log", "w")
 		
 		log(STATUS, "Giving the rogue hostapd one second to initialize ...")
