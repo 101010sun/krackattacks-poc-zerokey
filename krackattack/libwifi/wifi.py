09fa790fa7390e617916ab40621a33794b3500c9
611410066 <10sun19991119@gmail.com>
2023-05-15 20:15:23 +0800

    fix: debug

--- krackattack/libwifi/wifi.py
+++ krackattack/libwifi/wifi.py
@@ -227,7 +227,7 @@ class MitmSocket(L2Socket):
 			notdecoded='\x00\x6c' + self.get_channel_hex(channel) + '\xc0\x00\xa0\xc0\x00\x00')
 		L2Socket.send(self, rt/p)
 		if self.pcap: self.pcap.write(rt/p)
-		log(WARNING, "%s: Injected frame %s" % (self.iface, dot11_to_str(p)))
+		log(WARNING, "%s: Injected frame %s" % (self.iface, dot11_to_str(rt/p)))
 
 	def _strip_fcs(self, p):
 		# radiotap header flags 0x00...0: no used FCS failed
