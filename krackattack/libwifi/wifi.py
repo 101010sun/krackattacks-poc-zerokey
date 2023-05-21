d0a9c91ba4fc1a370739439ca092798eaa40c03a
611410066 <10sun19991119@gmail.com>
2023-05-21 16:27:19 +0800

    fix: packet add two radiotap

--- krackattack/libwifi/wifi.py
+++ krackattack/libwifi/wifi.py
@@ -225,9 +225,9 @@ class MitmSocket(L2Socket):
 		# 	len=18,
 		# 	present='Flags+Rate+Channel+dBm_AntSignal+Antenna', 
 		# 	notdecoded='\x00\x6c' + self.get_channel_hex(channel) + '\xc0\x00\xa0\xc0\x00\x00')
-		L2Socket.send(self, RadioTap()/p)
-		if self.pcap: self.pcap.write(RadioTap()/p)
-		log(WARNING, "%s: Injected frame %s" % (self.iface, dot11_to_str(RadioTap()/p)))
+		L2Socket.send(self, p)
+		if self.pcap: self.pcap.write(p)
+		log(WARNING, "%s: Injected frame %s" % (self.iface, dot11_to_str(p)))
 
 	def _strip_fcs(self, p):
 		# radiotap header flags 0x00...0: no used FCS failed
