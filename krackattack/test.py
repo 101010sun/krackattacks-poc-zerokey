e7490054fe7413765d1247f43bbac9fd68db80dd
611410066 <10sun19991119@gmail.com>
2023-06-02 17:20:07 +0800

    try: fix

--- krackattack/test.py
+++ krackattack/test.py
@@ -1,6 +1,6 @@
 from scapy.all import * 
 
-def send_80211_packet(destination_mac, from_mac, interface):
+def send_80211_packet(destination_mac, interface):
     # 创建L2Socket对象
     socket = L2Socket(iface=interface)
 
@@ -16,8 +16,7 @@ def send_80211_packet(destination_mac, from_mac, interface):
 
 # 指定要发送封包的目标MAC地址和网络接口
 destination_mac = "bc:ee:7b:e7:ab:54"  # 替换为目标设备的MAC地址
-from_mac = 'da:4a:77:65:f3:f5'
 interface = "wlan0"  # 替换为您的网络接口名称
 
 # 调用函数发送封包
-send_80211_packet(destination_mac, from_mac, interface)
+send_80211_packet(destination_mac, interface)
