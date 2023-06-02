65add2926682f655af5c4a5945121c812387a099
611410066 <10sun19991119@gmail.com>
2023-06-02 17:44:50 +0800

    try: send packet

--- krackattack/test.py
+++ krackattack/test.py
@@ -5,7 +5,7 @@ def send_80211_packet(destination_mac, interface):
     socket = L2Socket(iface=interface)
 
     # 构建要发送的封包
-    packet = RadioTap() / Dot11(type=0, subtype=8, addr1=destination_mac, addr2="00:11:22:33:44:55", addr3="00:11:22:33:44:55")
+    packet = RadioTap() / Dot11(type=2, subtype=4, addr1=destination_mac, addr2="00:11:22:33:44:55", addr3="00:11:22:33:44:55")
 
     try:
         # 发送封包
@@ -15,7 +15,7 @@ def send_80211_packet(destination_mac, interface):
         print("Failed to send packet:", e)
 
 # 指定要发送封包的目标MAC地址和网络接口
-destination_mac = "ff:ff:ff:ff:ff:ff"  # 替换为目标设备的MAC地址
+destination_mac = "bc:ee:7b:e7:ab:54"  # 替换为目标设备的MAC地址
 interface = "wlan0"  # 替换为您的网络接口名称
 
 # 调用函数发送封包
