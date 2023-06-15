62b2862163765aaac53a18c33909eb4c1bf02e5a
611410066 <10sun19991119@gmail.com>
2023-06-16 00:39:31 +0800

    fix: fake ap bug

--- krackattack/krack-all-zero-tk.py
+++ krackattack/krack-all-zero-tk.py
@@ -99,11 +99,10 @@ wpa={wpaver}
 wpa_key_mgmt={akms}
 wpa_pairwise={pairwise}
 rsn_pairwise={pairwise}
-rsn_ptksa_counters={ptksa_counters}
-rsn_gtksa_counters={gtksa_counters}
+
 
 wmm_enabled={wmmenabled}
-wmm_advertised={wmmadvertised}
+
 hw_mode=g
 auth_algs=3
 wpa_passphrase={password}"""
